f817b281c6df139ebcc9a0e659c05c3994d58c32
manette-r <73953040+manette-r@users.noreply.github.com>
2023-11-19 14:35:12 +0100

    Manon update
    Creation de ResultPage 
    Modification des fonctions generateurPref et valider (passage des valeurs de generateurPref à ResultPage)

---
+++ M2/Aide_a_la_decision/Aide a la décision/mariage_stable.py
@@ -0,0 +1,66 @@
+
+import random
+
+#généralement nb_etu=nb_eco
+def generateurPref(nb_etu,nb_eco):
+
+  etudiants = [random.sample(range(1, nb_etu+1), nb_etu) for i in range(nb_etu)]
+  ecole = [random.sample(range(1, nb_eco+1), nb_eco) for i in range(nb_eco)]
+
+  return etudiants,ecole
+
+#en entrée les listes qu'on peut obtenir en sortie de generateurPref
+def mariageSAlex(pref_etudiants, pref_ecole):
+
+  nb = len(pref_etudiants)
+  #liste d'attribution (index correspond à une ecole/eleve et valeur : 0 pas d'attribution, 1 : attribué)
+  etudiant_attribution = [0]*nb
+  ecole_attribution = [0]*nb
+
+  #affectation finale : les clefs sont les etudiants et les valeurs les ecoles
+  final_affectation = {}
+
+  #Tant que l'affectation est disponible
+  while 0 in etudiant_attribution:
+
+    #premier etudiant
+    i = 0
+    #on cherche un etudiant sans attribution
+    while etudiant_attribution[i] != 0:
+      i+=1
+    #on enlève son premier choix de la liste pour le traiter
+    w = pref_etudiants[i].pop(0)
+
+    #on vérifie que ce choix soit disponible pour affecter l'etudiant
+    if ecole_attribution[w-1] == 0:
+      final_affectation[w] = i+1
+      etudiant_attribution[i] = 1 # etudiant affecté
+      ecole_attribution[w-1] = 1 #ecole affecté
+      print("Etudiant",i+1,"affecté à l'école:",w)
+    else:
+      #si l'école avait déjà un étudiant attribué alors on les compare
+      print("Etudiant en attente:",i+1,"école :",w)
+      etudiant_attribue = final_affectation[w]
+      print("Etudiant actuel :",etudiant_attribue)
+
+      # On doit regarder les préférences de l'école
+      #Si l'ecole w préfère l'étudiant actuel à l'étudiant attribué
+      #Sinon l'étudiant attribué et l'école garde leur affectation
+      if (pref_ecole[w-1].index(etudiant_attribue)>pref_ecole[w-1].index(i+1)):
+
+        #on affecte et précise qu'ils ne sont pas disponibles
+        final_affectation[w] = i+1
+        etudiant_attribution[i] = 1
+        ecole_attribution[w-1] = 1
+
+        #l'étudiant qui était attribué devient disponible
+        etudiant_attribution[etudiant_attribue-1] = 0
+        print("Etudiant",i+1,"affecté à l'école:",w)
+
+  print("Les écoles ont toutes un élève : ",ecole_attribution) #[1,1,1]
+  print("Les élèves ont tous une école : ",etudiant_attribution) #[1,1,1]
+  return final_affectation
+
+def printAffectation(affectation, nb):
+  for k in list(range(1, nb+1)):
+    print("\nEleve ",k," a été admis dans l'école ",affectation.get(k),".")
--- M2/Aide_a_la_decision/Aide a la décision/parcoursoup.py
+++ M2/Aide_a_la_decision/Aide a la décision/parcoursoup.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 from PIL import Image, ImageTk  # install pillow with pip: pip install pillow
 import random
 from tkinter import StringVar
-
+from mariage_stable import *;
 class FirstPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -68,57 +68,122 @@ class ThirdPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-  
+        self.controller = controller 
+
         label = tk.Label(self,text="A vous de jouez !", font=("Aria Bold",40))
         label.place(x=230, y=30)
         texte_label = tk.Label(self, text="Selectionnez les criteres de votre choix !",font=("Aria ",20))
         texte_label.place(x=200, y=130)
         # Champs d'entrée
          
-        label1 = tk.Label(self, text="Entrez le nombre d'étudiants : ")
-        label1.place(x=220, y=250)
+        self.label1 = tk.Label(self, text="Entrez le nombre d'étudiants : ")
+        self.label1.place(x=220, y=250)
         
         #Initialisation des strings pour les afficher 
-        self.nb_pref = tk.IntVar()  
+        self.nb_pref = tk.StringVar()
         self.result_etudiants = tk.StringVar()
         self.result_ecoles = tk.StringVar()
         
-        var_entry = tk.Entry(self,textvariable=self.nb_pref)
-        var_entry.place(x=390, y=250)
+        self.var_entry = tk.Entry(self,textvariable=self.nb_pref)
+        self.var_entry.place(x=390, y=250)
           
-        generateur_button = tk.Button(self,text="Générer les préférences",command=self.generateurPref)
-        generateur_button.place(x=330,y=330)
+        generateur_button = tk.Button(self,text="Générer les préférences",command=self.generateurPrefContainer)
+        generateur_button.place(x=230,y=330)
 
         result_label_etudiants = tk.Label(self,textvariable=self.result_etudiants) 
         result_label_etudiants.place(x=10,y=300)
         result_label_ecoles = tk.Label(self,textvariable=self.result_ecoles) 
         result_label_ecoles.place(x=10,y=350)
 
-        #Button2 = tk.Button(self,font=("Arial", 15), text= "Lancer la simulation", command= lambda: controller.show_frame(ThirdPage))
-        #Button2.place(x=330, y=330)
+        Button2 = tk.Button(self,font=("Arial", 15), text= "Lancer la simulation", command= lambda: self.valider(self.result_etudiants, self.result_ecoles))
+        Button2.place(x=430, y=330)
      
         Button = tk.Button(self, text="Retour à la page principale", font=("Arial", 10), command=lambda: controller.show_frame(FirstPage))
         Button.place(x=10, y=450)
-    def generateurPref(self):
-            # Longueur 
-            nb_etu = int(self.nb_pref.get())
-            nb_eco = int(self.nb_pref.get())
-            print('ta grand mere')
-            # Générer
-            etudiants = [random.sample(range(1, nb_etu+1), nb_etu) for i in range(nb_etu)]
-            ecole = [random.sample(range(1, nb_eco+1), nb_eco) for i in range(nb_eco)]
-            self.result_etudiants.set(etudiants)
-            self.result_ecoles.set(ecole)  
+    def generateurPrefContainer(self):
+            
+            if(self.nb_pref.get().isnumeric()):
+                
+                self.var_entry.select_clear()
+                self.label1.config(fg='black')
 
-    def valider(entry1,entry2):
+                # Longueur 
+                nb_etu = int(self.nb_pref.get())
+                nb_eco = int(self.nb_pref.get())
+                # Générer
+                etudiants, ecole = generateurPref(nb_etu, nb_eco)
+
+                self.result_etudiants.set(etudiants)
+                self.result_ecoles.set(ecole)  
+            else : 
+                self.var_entry.select_clear()
+                self.label1.config(fg='red')
+
+    def valider(self,entry1,entry2):
         # Récupérer les valeurs des champs d'entrée
         valeur_entree1 = entry1.get()
         valeur_entree2 = entry2.get()
    
         # Afficher les valeurs récupérées
-        print("Valeur 1:", valeur_entree1)
+        print("Valeur 1:", valeur_entree1, ", type : ", type(valeur_entree1))
         print("Valeur 2:", valeur_entree2)
+
+        # On vérifie que la génération de Voeux est bien été lancée 
+        if( valeur_entree1 == None or valeur_entree1 == "" ): 
+            self.var_entry.select_clear()
+            self.label1.config(fg='red')
+        else : 
+            self.var_entry.select_clear()
+            self.label1.config(fg='black')
+
+            self.controller.etudiants_choix = valeur_entree1
+            self.controller.ecoles_choix = valeur_entree2
+            self.controller.frames[ResultPage].correct_label()
+            self.controller.show_frame(ResultPage)
   
+class ResultPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller 
+
+        label = tk.Label(self,text="Résultat admission !", font=("Aria Bold",40))
+        label.place(x=230, y=30)
+        texte_label = tk.Label(self, text="Selectionnez les criteres de votre choix !",font=("Aria ",20))
+        texte_label.place(x=200, y=130)
+        # Champs d'entrée
+         
+        label_choix_etudiants = tk.Label(self, text="Choix des étudiants : ")
+        label_choix_etudiants.place(x=50, y=250)
+
+        self.choix_etudiants = tk.Label(self, text=" choix ")
+        self.choix_etudiants.place(x=200, y=250)     
+
+        label_choix_ecoles = tk.Label(self, text="Choix des écoles : ")
+        label_choix_ecoles.place(x=50, y=350)
+
+        self.choix_ecoles = tk.Label(self, text=" choix ")
+        self.choix_ecoles.place(x=200, y=350)    
+
+        #Initialisation des strings pour les afficher 
+        self.nb_pref = tk.IntVar()  
+        self.result_etudiants = tk.StringVar()
+        self.result_ecoles = tk.StringVar()
+          
+
+        result_label_etudiants = tk.Label(self,textvariable=self.result_etudiants) 
+        result_label_etudiants.place(x=10,y=300)
+        result_label_ecoles = tk.Label(self,textvariable=self.result_ecoles) 
+        result_label_ecoles.place(x=10,y=350)
+     
+        Button = tk.Button(self, text="Retour à la page principale", font=("Arial", 10), command=lambda: controller.show_frame(FirstPage))
+        Button.place(x=10, y=450)
+
+    def correct_label(self):
+        self.choix_etudiants.config(text=self.controller.etudiants_choix)  
+        self.choix_ecoles.config(text=self.controller.ecoles_choix )  
+
+            
 
 class Application(tk.Tk):
 
@@ -133,7 +198,7 @@ class Application(tk.Tk):
         window.grid_columnconfigure(0, minsize=800)
  
         self.frames = {}
-        for F in (FirstPage, SecondPage,ThirdPage):
+        for F in (FirstPage, SecondPage,ThirdPage,ResultPage):
             frame = F(window, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
